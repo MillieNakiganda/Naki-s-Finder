987a39b4a88822bbdeacb3a0e14401db981c26f6
Millie Nakiganda <millienakiganda@gmail.com>
2022-06-30 17:09:58 +0300

    Chore(Read ME): Update Readme file

--- README.md
+++ README.md
@@ -3,14 +3,21 @@
 </div>
 
 <p>A client app for searching US cities by city or state.</p>
+<p>Check it out</p><a href='https://stalwart-gaufre-57e664.netlify.app/'> Naki's Finder</a>
 
 ## Getting started
+
 The client app requires Node.js 10.0+. Checkout the [docs](https://nodejs.org/en/) to see installation guidelines.
 
 #### Installation guidelines
+
 - Clone the repository
 - On your command line, switch to the app root directory
 - Install dependencies using `yarn install`
 - Run development server using `yarn start:dev`
 - Update production build using `yarn build`
 - Run the production build using `yarn start:prod`
+
+#### Wire Frame
+
+![Naki's Finder Wireframe](../assets/Naki's_Finder.png)
--- src/js/search.js
+++ src/js/search.js
@@ -4,8 +4,11 @@ import { obj } from './cities';
 
 const submitBtn = document.getElementById('search-btn');
 const input = document.querySelector('input');
+
 const returnMarkedCityState = (datavalue, inputText) => {
-  const regex = new RegExp(`(${inputText})`, 'gi');
+  const trailspac = inputText.trim();
+  const cleanSpaces = trailspac.replace(/\s+/g, ' ');
+  const regex = new RegExp(`(${cleanSpaces})`, 'gi');
   return datavalue.replace(regex, "<span style='background-color: #FFFF00'>$1</span>");
 };
 
@@ -13,7 +16,9 @@ const getData = () => {
   const inputText = document.getElementById('text-to-search').value;
 
   if (inputText !== '') {
-    const regex = new RegExp(inputText, 'ig');
+    const trailspac = inputText.trim();
+    const cleanSpaces = trailspac.replace(/\s+/g, ' ');
+    const regex = new RegExp(cleanSpaces, 'ig');
 
     let tableRow = ``;
 
@@ -21,9 +26,9 @@ const getData = () => {
       if ((data.city).match(regex) || (data.state).match(regex)) {
         tableRow += `<tr>`;
 
-        tableRow += `<td data-label='City'> ${returnMarkedCityState(data.city, inputText)}</td>`;
-        tableRow += `<td data-label='State'> ${returnMarkedCityState(data.state, inputText)} </td>`;
-        tableRow += `<td data-label='Population'> ${data.population} </td>`;
+        tableRow += `<td data-label='City'> ${returnMarkedCityState((data.city).trim(), inputText)}</td>`;
+        tableRow += `<td data-label='State'> ${returnMarkedCityState(data.state.trim(), inputText)} </td>`;
+        tableRow += `<td data-label='Population'> ${parseInt(data.population, 10).toLocaleString('en-US')} </td>`;
 
         // eslint-disable-next-line no-unused-expressions
         (parseInt(data.growth_from_2000_to_2013, 10) > 0)
@@ -44,6 +49,7 @@ const getData = () => {
 
 const clearTable = () => {
   document.getElementById('table_Cities').innerHTML = '';
+  document.getElementById('text-to-search').value = '';
   return false;
 };
 
