82af30a7f3ff927f53c7e6310e1eebae5e8689f0
Millie Nakiganda <millienakiganda@gmail.com>
2022-07-20 20:25:51 +0300

    chore: added text to description

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ const Description = () => (
         This is my <span className="italic">First Application</span> using React
         JS. <br /> <br />
         Although it is a very tiny app it still describes how to create components
-        in React, an awesome Javascript Library made by Facebook
+        in React, an awesome Javascript.
       </div>
 );
 export default Description;
